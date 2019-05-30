cca14fe80a1e99471023b9109759c54906028fad
drakejanier <50704353+drakejanier@users.noreply.github.com>
2019-05-31 00:24:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-  new_name = name + " para otot"
+  new_name = str(name) + " para otot"
   return render_template('index.html', name=new_name)
 
 
